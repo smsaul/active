a37e591c8cb64377e19920d57fdfb9fa6a8a00f7
Seth Saulnier <seth.saulnier@icloud.com>
2018-09-06 23:22:37 -0400

    what is greater than math?
    (i got my greater than symbol backwards)

--- paycheck_calculator/paycheck_calculator.py
+++ paycheck_calculator/paycheck_calculator.py
@@ -5,7 +5,7 @@ straight_pay = 15.00 #this is a constant.
 overtime_rate = straight_pay * 1.5
 after_tax = 0.75 #35% removed for taxes, etc., conservative result
 
-if ot_hours < 0:
+if ot_hours > 0:
   net_pay = ((straight_pay * 40) + (ot_hours * overtime_rate)) * after_tax
 else:
   net_pay = (straight_pay * hours_worked) * after_tax
