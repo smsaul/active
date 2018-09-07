fade5f15f90cc74952f07dce1da4b6b42fc967db
Seth Saulnier <seth.saulnier@icloud.com>
2018-09-06 23:21:39 -0400

    privacy changes
    changed pay rate and taxes to arbitrary, not-representing-me amounts

--- paycheck_calculator/paycheck_calculator.py
+++ paycheck_calculator/paycheck_calculator.py
@@ -1,10 +1,13 @@
 hours_worked = float(input("Hours worked: "))
 
 ot_hours = hours_worked - 40
-straight_pay = 19.95 #this is a constant.
+straight_pay = 15.00 #this is a constant.
 overtime_rate = straight_pay * 1.5
-after_tax = 0.65 #35% removed for taxes, etc., conservative result
+after_tax = 0.75 #35% removed for taxes, etc., conservative result
 
-net_pay = ((straight_pay * 40) + (ot_hours * overtime_rate)) * after_tax
+if ot_hours < 0:
+  net_pay = ((straight_pay * 40) + (ot_hours * overtime_rate)) * after_tax
+else:
+  net_pay = (straight_pay * hours_worked) * after_tax
 
 print('$%.2f' % net_pay )
